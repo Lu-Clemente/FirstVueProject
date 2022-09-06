0d1fa017113ab2e5521aee0cf4da0cf2ad85f38b
Luciene Clemente <luciene.clemente@unifei.edu.br>
2022-09-06 09:52:13 -0300

    First component

---
+++ src/components/FirstComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Hi {{ this.name }}! That's a Vue component!</h1>
+</template>
+
+<script>
+export default {
+    name: "FirstComponent",
+    data() {
+        return {
+            name: 'Lu'
+        }
+    }
+}
+</script>
