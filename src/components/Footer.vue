49322c247246aa81f060d2e66e527a4282a587e8
Luciene Clemente <luciene.clemente@unifei.edu.br>
2022-09-13 20:29:45 -0300

    Create Footer.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+  <!-- <font-awesome-icon v-bind:icon="icon" /> -->
+  <footer>
+    <p>Footer</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  data() {
+    return {
+      icon: "fa-solid fa-user-secret",
+    };
+  },
+};
+</script>
+
+<style scoped>
+footer {
+  height: 45px;
+  width: 100%;
+  padding: 1em;
+  border-top: 1px solid var(--color-border);
+}
+p {
+  text-align: center;
+}
+</style>
