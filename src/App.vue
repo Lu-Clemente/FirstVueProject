6eea135dc5dba58ef1b9beaeb2bdd242f85c7ea7
Luciene Clemente <luciene.clemente@unifei.edu.br>
2022-09-19 10:37:06 -0300

    update App

--- src/App.vue
+++ src/App.vue
@@ -4,44 +4,31 @@ import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <Header />
+  <MobileNavbar v-if="this.store.menuActive" />
+  <Home />
+  <Footer />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+import Header from "./components/Header.vue";
+import Home from "./components/Home.vue";
+import Footer from "./components/Footer.vue";
+import MobileNavbar from "./components/MobileNavbar.vue";
+import { store } from "./store/store.js";
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+export default {
+  name: "App",
+  components: {
+    Header,
+    Home,
+    Footer,
+    MobileNavbar,
+  },
+  data() {
+    return {
+      store,
+    };
+  },
+};
+</script>
