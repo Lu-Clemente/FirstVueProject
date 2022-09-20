cf3695ba9c222c6d0417a6f3815b15ec9ddc4b1f
Luciene Clemente <luciene.clemente@unifei.edu.br>
2022-09-20 11:12:50 -0300

    Router config

--- src/main.js
+++ src/main.js
@@ -1,13 +1,37 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faBars, faCircleNotch, faDoorOpen, faTerminal, faXmark } from '@fortawesome/free-solid-svg-icons'
-import { faFolderOpen, faAddressCard, faPaperPlane } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faBars,
+  faCircleNotch,
+  faDoorOpen,
+  faTerminal,
+  faXmark,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faFolderOpen,
+  faAddressCard,
+  faPaperPlane,
+} from "@fortawesome/free-regular-svg-icons";
 
-import './assets/main.css'
+import "./assets/main.css";
 
-library.add(faCircleNotch, faDoorOpen, faFolderOpen, faAddressCard, faPaperPlane, faBars, faTerminal, faXmark)
+import router from "@/routes/router";
 
-createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount('#app')
+library.add(
+  faCircleNotch,
+  faDoorOpen,
+  faFolderOpen,
+  faAddressCard,
+  faPaperPlane,
+  faBars,
+  faTerminal,
+  faXmark
+);
+
+createApp(App)
+  .use(router)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .mount("#app");
